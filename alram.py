81770a1bbd6831e5d14b1cfc7055d1c26073519f
Zundal <ajs15010120@gmail.com>
2021-10-21 17:39:10 +0900

    알람시계

--- alram.py
+++ alram.py
@@ -1,10 +1,22 @@
-def minute(n):
-    if n >= 0 or n <=59 :
-        if 0 > n - 45:
-            return n + 60 - 45
-        else :
-            return n - 45
+a, b= map(int, input().split())
+
+def hour(h):
+    if h == 0 :
+        h = 24
+    rHour = h - 1
     
-def hour(n):
-    if 0 <= n or n <= 23:
-        return n + 24 
+    return rHour
+    
+def calculator(h, m):
+    if m>=45:
+        normal = m 
+    else :
+        normal = 60 + m
+    realMinute = normal - 45
+    if realMinute>=15:
+        realHour = hour(h)
+    else :
+        realHour = h
+    return print(realHour, realMinute)
+
+calculator(a, b)
