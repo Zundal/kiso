2067df831944628d30ff632b6dedf927da15e9e2
Zundal <ajs15010120@gmail.com>
2022-01-24 23:32:46 +0900

    while python 0124

--- python/plusCycle.py
+++ python/plusCycle.py
@@ -1,39 +1,49 @@
-# 더하기 사이클
-# https://www.acmicpc.net/problem/1110
 '''
-    예시
-    26 => 
-        2 + 6 = 8
-        68
-        6 + 8 = 14
-        84
-        8 + 4 = 12
-        42 
-        4 + 2 = 6
-        26
-    
+0보다 크거나 같고, 99보다 작거나 같은 정수가 주어질 때 다음과 같은 연산을 할 수 있다. 
+먼저 주어진 수가 10보다 작다면 앞에 0을 붙여 두 자리 수로 만들고, 각 자리의 숫자를 더한다. 
+그 다음, 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙이면 새로운 수를 만들 수 있다. 
+
+다음 예를 보자.
+
+    26부터 시작한다. 
+    2+6 = 8이다. 새로운 수는 68이다. 
+    6+8 = 14이다. 새로운 수는 84이다. 
+    8+4 = 12이다. 새로운 수는 42이다. 
+    4+2 = 6이다. 새로운 수는 26이다.
+
+위의 예는 4번만에 원래 수로 돌아올 수 있다. 
+따라서 26의 사이클의 길이는 4이다.
+
+N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작성하시오.
 '''
 num = int(input())
+first_ = num
+cnt = 0
 temp = 0
-def plusCycle(num):
-    arr = []
-    if num>=0 and num<=99 :
-        arr = list(str(num))
-        
-        temp = int(arr[0]) + int(arr[1])
-        
-        print(str(arr[1])+str(str(temp)[::-1]))
-    #     if num<10:
-            
-    #         pass
-    #     else :
-            
-    #         pass
-        
-    #     result = 0
-    #     return result 
-    # else:
-    #     return False
-    pass
 
-plusCycle(num)
+if num >= 0 and num <= 99 :
+    while (True):
+        
+        if(temp != 0) :
+            if first_ == int(temp) :
+                break
+            else :
+                num = int(temp)
+            
+        if num < 10 :
+            temp = str(num) + str(num)
+        else :
+            store = str(num)
+            first = int(store[0])
+            second = int(store[1])
+            
+            result = str(first + second)
+            
+            if len(result) > 1 :
+                temp = store[1] + result[1]
+            else :
+                temp = store[1] + result
+                
+        cnt = cnt + 1
+
+print(cnt)
