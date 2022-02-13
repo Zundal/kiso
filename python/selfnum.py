152ac2a27f165a8f67d4ff00d6b7338157671e96
Zundal <ajs15010120@gmail.com>
2022-02-13 10:56:47 +0900

    test study

--- python/selfnum.py
+++ python/selfnum.py
@@ -1,22 +1,9 @@
-'''while(True):
-    
-    if result > 10000:
-        break;
-    else :
-        print(result)
-
-            d(75) = 75 + 7 + 5 = 87
-                    
-
-        '''
-
 def selfNum(n):
     return n+sum([int(i) for i in str(n)])
 
 arr=[0]*10000
 for i in range(10000):
     if(selfNum(i)<10000):
-        print(selfNum(i))
         arr[selfNum(i)]=1
 
 for i in range(10000):
