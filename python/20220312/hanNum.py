ba81875ee92066b8436b6f545aab63e11664f4fa
Zundal <ajs15010120@gmail.com>
2022-04-25 23:44:16 +0900

    up

--- python/20220312/hanNum.py
+++ python/20220312/hanNum.py
@@ -1,15 +1,24 @@
+# 한수란
+#   각 자리수가 등차수열로 이루어진 수
+# 123 147 555 공차가 일정하니 한수이다
+# 1 ~ 99 까지는 한수 이다
+# 그 이후의 수는 한수이기도 아니기도 하다
+# 그러면 한수를 구하는 공식을 구해보자 
 def getDifference(num):
-    checkerArr = [False] * num
-    differenceCount = 0    
+    if num < 100:
+        return num
+    else:        
+        checkerArr = [False] * num
+        differenceCount = 0    
 
-    for idx in range(num):
-        checkerArr[idx] = numberDiffernceChecke(str(idx+1))
+        for idx in range(num):
+            checkerArr[idx] = numberDiffernceChecke(str(idx+1))
 
-    for val in checkerArr:
-        if(val==True):
-            differenceCount = differenceCount + 1
+        for val in checkerArr:
+            if(val==True):
+                differenceCount = differenceCount + 1
 
-    return differenceCount
+        return differenceCount
 
 def numberDiffernceChecke(num):
     result = False 
