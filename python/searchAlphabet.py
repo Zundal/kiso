8621d6812891f33c8385c89afba4d7ae15a85482
Zundal <ajs15010120@gmail.com>
2022-05-06 12:28:25 +0900

    문자열 문제

--- python/searchAlphabet.py
+++ python/searchAlphabet.py
@@ -4,8 +4,9 @@ result = []
 ask = 97
 for i in range(26):
     if chr(ask) in txt:
-        result.append(txt.index(chr(ask)))
+        result.append(str(txt.index(chr(ask))))
     else:
-        result.append(-1)
+        result.append('-1')
     ask+=1
-print(result)
+
+print(" ".join(result))
